14244737ebed9f65a3fde6c85c524d19df83df0a
hikikomori <64779938+hik1komori@users.noreply.github.com>
2025-11-20 01:03:17 +0500

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_IDS = [5494287847, 6531897948]
 CODES_CHANNEL = "https://www.instagram.com/k1no_bot1?igsh=MTVvaW5sbjkxbmZ3cQ%3D%3D&utm_source=qr" 
 # ID архив-канала (где хранятся фильмы) (например: -1001234567890)
-ARCHIVE_CHANNEL_ID = -1003022803360
+ARCHIVE_CHANNEL_ID = --1003310091087
 
 # Обязательные каналы для подписки (формат: {"channel_id": "@username"})
 REQUIRED_CHANNELS = {
