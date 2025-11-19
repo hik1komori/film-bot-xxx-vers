50bca616acf9f17e423fcd7a1f4bce538630acd3
hikikomori <64779938+hik1komori@users.noreply.github.com>
2025-11-19 23:20:02 +0500

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # ID администраторов (можно получить через @userinfobot)
 ADMIN_IDS = [5494287847, 6531897948]
-CODES_CHANNEL = "https://t.me/proda_kod" 
+CODES_CHANNEL = "https://www.instagram.com/k1no_bot1?igsh=MTVvaW5sbjkxbmZ3cQ%3D%3D&utm_source=qr" 
 # ID архив-канала (где хранятся фильмы) (например: -1001234567890)
 ARCHIVE_CHANNEL_ID = -1003022803360
 
